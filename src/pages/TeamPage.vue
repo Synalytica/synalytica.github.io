907b8432a6b93a54ee0a8658c6d8edc28e73b8eb
Pk13055 <Pk13055@users.noreply.github.com>
2023-10-28 15:15:11 +0400

    [Addition] Team page

--- src/pages/TeamPage.vue
+++ src/pages/TeamPage.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page padding>
-    <div class="row items-center text-center">
+    <div class="row q-mb-xl items-center justify-center text-center">
       <div class="col">
         <h3 class="text-h3">
           <q-icon name="mdi-crowd" size="lg" left />
@@ -8,27 +8,164 @@
         </h3>
       </div>
     </div>
-    <div class="row items-center text-center">
-      <div class="col">
-        <!-- Mahesh -->
+    <div class="row flex-center q-mb-xl">
+      <!-- founders -->
+      <div class="col-xs-12 col-md-auto q-mr-lg">
+        <q-card class="founding-member">
+          <q-img src="team/mahesh.png">
+            <div class="absolute-bottom text-left name-header">
+              <div class="text-h6">Mahesh S</div>
+              <div class="text-overline text-uppercase">Founder</div>
+            </div>
+          </q-img>
+          <q-card-actions class="text-right text-uppercase text-overline">
+            <span class="text-body2">CEO</span>
+            <q-space />
+            <q-btn flat icon="mdi-linkedin" />
+            <q-btn flat icon="mdi-email" />
+          </q-card-actions>
+        </q-card>
       </div>
-      <div class="col">
-        <!-- Pratik -->
+      <div class="col-xs-12 col-md-auto q-ml-lg">
+        <q-card class="founding-member">
+          <q-img src="team/pratik.png">
+            <div class="absolute-bottom text-left name-header">
+              <div class="text-h6">Pratik K</div>
+              <div class="text-overline text-uppercase">Founder</div>
+            </div>
+          </q-img>
+          <q-card-actions class="text-right text-uppercase text-overline">
+            <span class="text-body2">CTO</span>
+            <q-space />
+            <q-btn flat icon="mdi-linkedin" />
+            <q-btn flat icon="mdi-github" />
+          </q-card-actions>
+        </q-card>
       </div>
     </div>
-    <div class="row items-center text-center">
-      <div class="col">
-        <!-- Sanjay -->
+    <div class="row flex-center q-mb-lg">
+      <!-- core team -->
+      <div class="col-xs-12 col-md-auto q-mr-lg">
+        <q-card class="core-member">
+          <q-img src="team/sanjay.png">
+            <div class="absolute-bottom text-right name-header">
+              <div class="text-h6">Sanjay K</div>
+              <div class="text-overline text-uppercase">Core Team</div>
+            </div>
+          </q-img>
+          <q-card-actions class="text-overline text-uppercase">
+            Systems Architect
+          </q-card-actions>
+        </q-card>
       </div>
-      <div class="col">
-        <!-- Gurkirat -->
+      <div class="col-xs-12 col-md-auto q-ml-lg">
+        <q-card class="core-member">
+          <q-img src="team/gurkirat.png">
+            <div class="absolute-bottom text-right name-header">
+              <div class="text-h6">Gurkirat S</div>
+              <div class="text-overline text-uppercase">Core Team</div>
+            </div>
+          </q-img>
+          <q-card-actions class="text-overline text-uppercase">
+            Full-Stack Engineer
+          </q-card-actions>
+        </q-card>
       </div>
     </div>
-    <div class="row items-center text-center">
-      <div class="col">
-        <!-- Pratham -->
+    <div class="row flex-center q-mb-md">
+      <!-- team -->
+      <div class="col-xs-12 col-md-auto q-mr-lg">
+        <q-card class="team-member">
+          <q-img src="team/sadiq.png">
+            <div class="absolute-bottom text-right name-header">
+              <div class="text-h6">Sadiq A</div>
+              <div class="text-overline text-uppercase">Engg. Team</div>
+            </div>
+          </q-img>
+          <q-card-actions class="text-overline text-uppercase">
+            AI Engineer
+          </q-card-actions>
+        </q-card>
+      </div>
+      <div class="col-xs-12 col-md-auto q-mx-lg">
+        <q-card class="team-member">
+          <!-- <q-img src="team/samruddhi.png"> -->
+          <q-img src="team-woman.jpg">
+            <div class="absolute-bottom text-right name-header">
+              <div class="text-h6">Samruddhi S</div>
+              <div class="text-overline text-uppercase">UI/UX</div>
+            </div>
+          </q-img>
+          <q-card-actions class="text-overline text-uppercase">
+            Designer
+          </q-card-actions>
+        </q-card>
+      </div>
+      <div class="col-xs-12 col-md-auto q-ml-lg">
+        <q-card class="team-member">
+          <!-- <q-img src="team/shamees.png"> -->
+          <q-img src="team-man.jpg">
+            <div class="absolute-bottom text-right name-header">
+              <div class="text-h6">Shamees R</div>
+              <div class="text-overline text-uppercase">Engg. Team</div>
+            </div>
+          </q-img>
+          <q-card-actions class="text-overline text-uppercase">
+            Full-Stack Engineer
+          </q-card-actions>
+        </q-card>
+      </div>
+    </div>
+    <div class="row flex-center q-mb-sm">
+      <div class="col xs-12 col-md-auto q-mr-lg">
+        <q-card class="intern-member">
+          <q-img src="team/noel.png">
+            <div class="absolute-bottom text-right name-header">
+              <div class="text-h6">Noel J</div>
+              <div class="text-overline text-uppercase">Intern</div>
+            </div>
+          </q-img>
+          <q-card-actions class="text-overline text-uppercase">
+            Software Engineer
+          </q-card-actions>
+        </q-card>
+      </div>
+      <div class="col xs-12 col-md-auto q-ml-lg">
+        <q-card class="intern-member">
+          <q-img src="team-man.jpg">
+            <!-- <q-img src="team/nikhil.png"> -->
+            <div class="absolute-bottom text-right name-header">
+              <div class="text-h6">Nikhil V</div>
+              <div class="text-overline text-uppercase">Intern</div>
+            </div>
+          </q-img>
+          <q-card-actions class="text-overline text-uppercase">
+            Software Engineer
+          </q-card-actions>
+        </q-card>
       </div>
     </div>
   </q-page>
 </template>
 <script setup></script>
+<style scoped>
+.name-header {
+  background-color: rgba(0, 0, 0, 0.3);
+}
+.founding-member {
+  border-radius: 1rem;
+  width: 420px;
+}
+.core-member {
+  border-radius: 1rem;
+  width: 350px;
+}
+.team-member {
+  border-radius: 1rem;
+  width: 300px;
+}
+.intern-member {
+  border-radius: 1rem;
+  width: 256px;
+}
+</style>
