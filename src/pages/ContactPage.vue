47b3b3c2ca9522faf88900b2a0fc3b4ba73f8315
Pk13055 <Pk13055@users.noreply.github.com>
2024-04-15 10:09:21 -0400

    [Addition] easter egg

--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -13,6 +13,11 @@
         <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeq4IvaCETa0Ty-mVR49VprCh2T3ymOaqHkU7Nzkvv4kEDZ1w/viewform?embedded=true" width="800" height="1452" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
       </div>
     </div>
+    <div class="row items-center text-center">
+      <div class="col">
+        <iframe src="https://widget.nicehash.com/countdown/btc-halving-2024-05-10-12-00" width="400" height="350" scrolling="no" id="nhiframe"></iframe>
+      </div>
+    </div>
   </q-page>
 </template>
 <script setup></script>
